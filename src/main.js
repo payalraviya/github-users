730812003048784128433131b73648b2cbab18c8
Payal17122000 <raviyapayal17@gmail.com>
2024-06-01 18:24:50 +0530

    fix: remove unwanted code

--- src/main.js
+++ src/main.js
@@ -1,15 +1,9 @@
 
 import { createApp } from 'vue'
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap"
 import App from './App.vue'
-// import router from './router'
-import store from './store/store'
 import '@/assets/main.css';
 const app = createApp(App)
 
-// app.use(router)
-app.use(store)
 
 
 app.mount('#app')
